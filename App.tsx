7458d8d9b84bbc058a18b9371cf6a1fd5dc2dc89
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 22:02:23 -0300

    adicionando ícone e Switch

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import "./styles/global.css"
-import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
- 
+import { Text, View, TextInput, TouchableOpacity, Image, Switch } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <View className="bg-gray-900 px-4 w-full h-full pt-7">
+    <View className={`${isDarkMode ? 'bg-gray-900' : 'bg-gray-400'} px-4 w-full h-full pt-7`}>
       <Image
         source={require("./assets/fiap_logo.png")}
         className="w-48 h-48 mx-auto"
@@ -40,20 +43,33 @@ export default function App() {
         </Text>
       </TouchableOpacity>
 
-      <Text className="text-xl font-bold text-gray-400 text-center p-5 opacity-80">
+      <Text className={`text-xl font-bold ${isDarkMode ? 'text-gray-400' : 'text-gray-800'} text-center p-5 opacity-80`}>
         ---------- or sign with ---------- 
       </Text>
-
-      <TouchableOpacity className="bg-gray-200 active:bg-gray-700 rounded-xl px-6 py-4 mt-4 shadow-lg">
-        <Text className="text-gray-500 text-center font-bold text-lg">
+      <TouchableOpacity className="bg-gray-200 rounded-xl px-6 py-4 mt-4 shadow-lg flex-row items-center justify-center">
+        <AntDesign name="google" size={24} color="#DB4437" className="mr-3" />
+        <Text className="text-gray-500 font-bold text-lg">
           Continue with Google
         </Text>
       </TouchableOpacity>
 
-      <Text className="text-xl font-bold text-gray-400 text-center p-5 opacity-80">
+      <Text className={`text-xl font-bold ${isDarkMode ? 'text-gray-400' : 'text-gray-800'} text-center p-5 opacity-80`}>
         Create an account
       </Text>
+      
+      <View className="flex-row justify-between items-center">
+        <Text className={`text-xl font-bold ${isDarkMode ? 'text-gray-400' : 'text-gray-800'} mb-2`}>
+          {isDarkMode ? "Dark mode" : "Light mode"}
+        </Text>
 
+        <Switch
+          value={isDarkMode}
+          onValueChange={setIsDarkMode}
+          trackColor={{ false: "#767577", true: "#e11d48" }}
+          thumbColor={isDarkMode ? "#9ca3af" : "#f4f3f4"}
+        />
+
+      </View>
     </View>
   );
 }
